e7dda9018109233096f992c3526c76aa33921193
Jin <jinyongjeong07@gmail.com>
2019-06-28 10:12:05 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// /Esports at UGA/slick/slick.js
+$(document).ready(function(){
+    $('.allgame').slick({
+        infinite:true,
+        slidesToShow: 3,
+        slidesToScroll:3,
+        appendArrows: $('.allgame')
+        // depends does it auto fit
+    });
+});
